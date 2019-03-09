4d09246ff167e7d0f4a66b8a131dfea9d1c80dc0
ludko_pro <antoan.yanev5@gmail.com>
2019-03-10 00:06:25 +0200

    Added login validation

--- Calendar/Form1.cs
+++ Calendar/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace Calendar {
     public partial class Form1 : Form {
@@ -31,19 +32,44 @@ namespace Calendar {
             dbCon.Open();
 
             using (dbCon) {
-                string name = NameBox.Text;
-                string surname = SurnameBox.Text;
-                string birthdate = BirthdateBox.Text;
-                string gender = GenderBox.Text;
-                string city = CityBox.Text;
+                string[] info = new string[] { NameBox.Text, SurnameBox.Text, BirthdateBox.Text, GenderBox.Text, CityBox.Text };
 
-                string values = $"VALUES ('{name}', '{surname}', '{birthdate}', '{gender}', '{city}')";
+                if (CheckNull(info) && CheckDate(info[2])) {
+                    string values = $"VALUES ('{info[0]}', '{info[1]}', '{info[2]}', '{info[3]}', '{info[4]}')";
 
-                SqlCommand command = new SqlCommand("INSERT INTO USERS (Name, Surname, Birthdate, Gender, City)" + values, dbCon);
-                command.ExecuteScalar();
+                    SqlCommand command = new SqlCommand("INSERT INTO USERS (Name, Surname, Birthdate, Gender, City)" + values, dbCon);
+                    command.ExecuteScalar();
+                } 
             }
 
             dbCon.Close();
+        }
+
+        private bool CheckNull(string[] input) {
+            bool ok = true;
+
+            foreach (string str in input) {
+                if (str == String.Empty) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            return ok;
+        }
+
+        private bool CheckDate(string input) {
+            bool ok = true;
+
+            Regex rx = new Regex(@"([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))");
+
+            MatchCollection matches = rx.Matches(input);
+
+            if (matches.Count != 1) {
+                ok = false;
+            }
+
+            return ok;
         }
     }
 }
